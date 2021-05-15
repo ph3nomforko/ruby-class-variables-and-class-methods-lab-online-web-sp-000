ad280699d96b5d7a2b8dae0da90b4e0b2338ed0c
Codi Kraus <codi.kraus@gmail.com>
2021-05-15 19:34:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,13 @@
 class Song
-  attr_reader :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
 
   @@count = 0
-  def
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+  end
+
+  def count
+    @@count
   end
 end
